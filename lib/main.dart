40156967341f383fb855b5ee386faf1c75d026bc
Abraham Maleko <abrahammaleko@gmail.com>
2023-04-04 03:33:33 +0300

    added navigation container

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,74 @@
 import 'package:flutter/material.dart';
+import 'my_notes.dart';
 
 void main() {
-  runApp(const MyNotes());
+  runApp(const MyApp());
 }
 
-class MyNotes extends StatelessWidget {
-  const MyNotes({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'My Notebook',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('My Notebook'),
-        ),
-        body: Container(
-          margin: const EdgeInsets.fromLTRB(10, 30, 10, 10),
-          child: Card(
-            color: const Color.fromARGB(255, 255, 238, 234),
-            child: SizedBox(
-              height: 600,
-              child: Form(
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(10, 15, 8, 15),
-                  child: Column(
-                    children: [
-                      TextFormField(
-                        decoration: const InputDecoration(
-                            labelText: 'Subject',
-                            labelStyle: TextStyle(fontSize: 18.0),
-                            hintText: 'i.e learn Flutter'),
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      TextFormField(
-                        maxLines: 18,
-                        decoration: const InputDecoration(
-                            hintText: 'i.e what do you want to write about'),
-                      ),
-                      const SizedBox(
-                        height: 30,
-                      ),
-                      ElevatedButton(
-                        style: ButtonStyle(
-                          textStyle: MaterialStateProperty.all(
-                              const TextStyle(fontSize: 20)),
-                          padding: MaterialStateProperty.all(
-                              const EdgeInsets.fromLTRB(20, 15, 20, 15)),
-                        ),
-                        onPressed: () {},
-                        child: const Text('Save'),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
+    return const MaterialApp(
+      home: NavigationContainer(),
+    );
+  }
+}
+
+class NavigationContainer extends StatefulWidget {
+  const NavigationContainer({super.key});
+
+  @override
+  State<NavigationContainer> createState() => _NavigationContainerState();
+}
+
+class _NavigationContainerState extends State<NavigationContainer> {
+  int currentIndex = 0; //stores the index of the home page
+
+  @override
+  Widget build(BuildContext context) {
+    Widget page;
+
+    switch (currentIndex) {
+      case 0:
+        page = const Text('My Notes');
+        break;
+      case 1:
+       page = const MyNotes();
+        break;
+      case 2:
+        page = const Text('Saved Notes');
+        break;
+      default:
+        throw UnimplementedError('no widget for $currentIndex');
+    }
+
+    return Scaffold(
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+        selectedIndex: currentIndex,
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(Icons.explore),
+            label: 'Notes',
           ),
-        ),
+          NavigationDestination(
+            icon: Icon(Icons.add_chart_outlined),
+            label: 'New Note',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.bookmark),
+            icon: Icon(Icons.bookmark_border),
+            label: 'Saved',
+          )
+        ],
       ),
+      body: Container(child: page),
     );
   }
 }
