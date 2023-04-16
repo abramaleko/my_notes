6fff41f1e1af589f5027256efc238da7cf393098
Abraham Maleko <abrahammaleko@gmail.com>
2023-04-16 22:33:06 +0300

    fixed overflow of form

--- lib/my_notes.dart
+++ lib/my_notes.dart
@@ -50,61 +50,61 @@ class _MyNotesState extends State<MyNotes> {
         ),
         body: Container(
           margin: const EdgeInsets.fromLTRB(10, 10, 10, 10),
-          child: Card(
+          child: SingleChildScrollView(
             child: SizedBox(
-              height: 640,
-              child: Form(
-                key: _formKey,
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(10, 15, 8, 0),
-                  child: Column(
-                    children: [
-                      TextFormField(
-                        controller: _topic,
-                        decoration: const InputDecoration(
-                            labelText: 'Subject',
-                            labelStyle: TextStyle(fontSize: 18.0),
-                            hintText: 'i.e learn Flutter'),
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please enter the subject header';
-                          }
-                          return null;
-                        },
+            height: 640,
+            child: Form(
+              key: _formKey,
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(10, 15, 8, 0),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      controller: _topic,
+                      decoration: const InputDecoration(
+                          labelText: 'Subject',
+                          labelStyle: TextStyle(fontSize: 18.0),
+                          hintText: 'i.e learn Flutter'),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter the subject header';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      controller: _content,
+                      maxLines: 20,
+                      decoration: const InputDecoration(
+                          hintText: 'i.e what do you want to write about'),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter the content';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    ElevatedButton(
+                      style: ButtonStyle(
+                        textStyle: MaterialStateProperty.all(
+                            const TextStyle(fontSize: 20)),
+                        padding: MaterialStateProperty.all(
+                            const EdgeInsets.fromLTRB(20, 10, 20, 10)),
                       ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      TextFormField(
-                        controller: _content,
-                        maxLines: 20,
-                        decoration: const InputDecoration(
-                            hintText: 'i.e what do you want to write about'),
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please enter the content';
-                          }
-                          return null;
-                        },
-                      ),
-                      const SizedBox(
-                        height: 30,
-                      ),
-                      ElevatedButton(
-                        style: ButtonStyle(
-                          textStyle: MaterialStateProperty.all(
-                              const TextStyle(fontSize: 20)),
-                          padding: MaterialStateProperty.all(
-                              const EdgeInsets.fromLTRB(20, 15, 20, 15)),
-                        ),
-                        onPressed: saveInfo,
-                        child: const Text('Save'),
-                      ),
-                    ],
-                  ),
+                      onPressed: saveInfo,
+                      child: const Text('Save'),
+                    ),
+                  ],
                 ),
               ),
             ),
+          ),
           ),
         ),
       ),
