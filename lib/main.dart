b9243337a7c760d09caa2a4a5bdbd13feb10f66a
Abraham Maleko <abrahammaleko@gmail.com>
2023-05-04 14:37:18 +0300

    rewritten the app using go router package

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:my_notes/note.dart';
 import 'package:my_notes/note_provide.dart';
 import 'package:provider/provider.dart';
 import 'my_notes.dart';
 import 'notes_list.dart';
+import 'package:go_router/go_router.dart';
+
+// GoRouter configuration
+final _router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const NotesList(),
+    ),
+    GoRoute(
+      path: '/create-note',
+      builder: (context, state) => const MyNotes(),
+    ),
+    GoRoute(
+      path: '/note',
+      builder: (context, state) =>  Note(),
+      ),
+  ],
+);
 
 void main() {
-  runApp(const MyApp());
+  runApp(MultiProvider(providers: [
+    ChangeNotifierProvider<NoteProvider>(create: (context) => NoteProvider()),
+  ], child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -13,65 +35,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: ChangeNotifierProvider<NoteProvider>(
-          create: (context) => NoteProvider(),
-          child: const NavigationContainer()),
+    return MaterialApp.router(
+      title: 'My notes',
+      routerConfig: _router,
     );
   }
 }
 
-class NavigationContainer extends StatefulWidget {
-  const NavigationContainer({Key? key}) : super(key: key);
-
-  @override
-  State<NavigationContainer> createState() => _NavigationContainerState();
-}
-
-class _NavigationContainerState extends State<NavigationContainer> {
-  @override
-  Widget build(BuildContext context) {
-    final noteProvider = Provider.of<NoteProvider>(context);
-    Widget page;
-
-    switch (noteProvider.currentIndex) {
-      case 0:
-        //tocheck
-        page = const NotesList();
-        break;
-      case 1:
-        page = const MyNotes();
-        break;
-      case 2:
-        page = const Text('Saved Notes');
-        break;
-      default:
-        throw UnimplementedError('no widget for ${noteProvider.currentIndex}');
-    }
-
-    return Scaffold(
-      bottomNavigationBar: NavigationBar(
-        onDestinationSelected: (int index) {
-          noteProvider.updatePage(index);
-        },
-        selectedIndex: noteProvider.currentIndex,
-        destinations: const [
-          NavigationDestination(
-            icon: Icon(Icons.explore),
-            label: 'Notes',
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.add_chart_outlined),
-            label: 'New Note',
-          ),
-          NavigationDestination(
-            selectedIcon: Icon(Icons.bookmark),
-            icon: Icon(Icons.bookmark_border),
-            label: 'Saved',
-          )
-        ],
-      ),
-      body: Container(child: page),
-    );
-  }
-}
--- lib/my_notes.dart
+++ lib/my_notes.dart
@@ -1,7 +1,9 @@
 import 'dart:math';
+import 'package:go_router/go_router.dart';
 import 'package:my_notes/note_provide.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
+import './navigation_container.dart';
 
 class MyNotes extends StatelessWidget {
   const MyNotes({super.key});
@@ -22,74 +24,72 @@ class MyNotes extends StatelessWidget {
           'topic': topic.text,
           'content': content.text,
         };
-        
-        topic.clear();
-         content.clear();
-         noteProvider.addNote(data); //add note to the list
 
+        topic.clear();
+        content.clear();
+        noteProvider.addNote(data); //add note to the list
+        context.go('/');
       }
     }
 
-    return MaterialApp(
-      title: 'My Notebook',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Create Note'),
-        ),
-        body: Container(
-          margin: const EdgeInsets.fromLTRB(10, 10, 10, 10),
-          child: SingleChildScrollView(
-            child: SizedBox(
-              height: 640,
-              child: Form(
-                key: formKey,
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(10, 15, 8, 0),
-                  child: Column(
-                    children: [
-                      TextFormField(
-                        controller: topic,
-                        decoration: const InputDecoration(
-                            labelText: 'Subject',
-                            labelStyle: TextStyle(fontSize: 18.0),
-                            hintText: 'i.e learn Flutter'),
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please enter the subject header';
-                          }
-                          return null;
-                        },
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Create Note'),
+      ),
+      bottomNavigationBar: const NavigationContainer(),
+      body: Container(
+        margin: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+        child: SingleChildScrollView(
+          child: SizedBox(
+            height: 640,
+            child: Form(
+              key: formKey,
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(10, 15, 8, 0),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      controller: topic,
+                      decoration: const InputDecoration(
+                          labelText: 'Subject',
+                          labelStyle: TextStyle(fontSize: 18.0),
+                          hintText: 'i.e learn Flutter'),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter the subject header';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      controller: content,
+                      maxLines: 20,
+                      decoration: const InputDecoration(
+                          hintText: 'i.e what do you want to write about'),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter the content';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    ElevatedButton(
+                      style: ButtonStyle(
+                        textStyle: MaterialStateProperty.all(
+                            const TextStyle(fontSize: 20)),
+                        padding: MaterialStateProperty.all(
+                            const EdgeInsets.fromLTRB(20, 10, 20, 10)),
                       ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      TextFormField(
-                        controller: content,
-                        maxLines: 20,
-                        decoration: const InputDecoration(
-                            hintText: 'i.e what do you want to write about'),
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please enter the content';
-                          }
-                          return null;
-                        },
-                      ),
-                      const SizedBox(
-                        height: 30,
-                      ),
-                      ElevatedButton(
-                        style: ButtonStyle(
-                          textStyle: MaterialStateProperty.all(
-                              const TextStyle(fontSize: 20)),
-                          padding: MaterialStateProperty.all(
-                              const EdgeInsets.fromLTRB(20, 10, 20, 10)),
-                        ),
-                        onPressed: saveInfo,
-                        child: const Text('Save'),
-                      ),
-                    ],
-                  ),
+                      onPressed: saveInfo,
+                      child: const Text('Save'),
+                    ),
+                  ],
                 ),
               ),
             ),
